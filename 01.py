0b82f532fe68745f1f51990168c71c395323593e
Anže Pečar <anze@pecar.me>
2023-11-30 23:22:49 +0000

    Update 01.py template

--- 01.py
+++ 01.py
@@ -6,12 +6,12 @@ aoc = AoC(day=int(Path(__file__).stem))
 
 
 def part1(input):
-    return 0
+    return None
 
 
 def part2(input):
-    return 0
+    return None
 
 
 aoc.submit_p1(part1(aoc.get_input()))
-# aoc.submit_p2(part2(aoc.get_input()))
+aoc.submit_p2(part2(aoc.get_input()))
