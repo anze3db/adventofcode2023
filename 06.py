c8f49ee4b02f5aaf34a8dfeb8ce46687731cb584
Anže Pečar <anze@pecar.me>
2023-12-06 08:18:09 +0000

    Solve day 6

---
+++ 06.py
@@ -0,0 +1,54 @@
+from textwrap import dedent
+
+from utils import AoC
+
+aoc = AoC()
+
+
+def part1(inp):
+    times = list(map(int, inp[0].split(": ")[1].split()))
+    distances = list(map(int, inp[1].split(": ")[1].split()))
+    num = 1
+    for time, distance in zip(times, distances):
+        options = 0
+        for i in range(1, time):
+            if (time - i) * i > distance:
+                options += 1
+        num *= options
+    return num
+
+
+def part2(inp):
+    time = int(inp[0].split(": ")[1].replace(" ", ""))
+    distance = int(inp[1].split(": ")[1].replace(" ", ""))
+    num = 1
+
+    options = 0
+    for i in range(1, time):
+        if (time - i) * i > distance:
+            options += 1
+    num *= options
+    return num
+
+
+assert (
+    part1(
+        dedent(
+            """Time:      7  15   30
+Distance:  9  40  200"""
+        ).splitlines()
+    )
+    == 288
+)
+aoc.submit_p1(part1(aoc.get_input()))
+
+assert (
+    part2(
+        dedent(
+            """Time:      7  15   30
+Distance:  9  40  200"""
+        ).splitlines()
+    )
+    == 71503
+)
+aoc.submit_p2(part2(aoc.get_input()))
