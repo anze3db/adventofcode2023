549dae6fde7a439c0f4bbdfb4fdb4a2abc4cd1fe
Anže Pečar <anze@pecar.me>
2023-12-19 07:33:10 +0000

    Add templates for last 5

---
+++ 24.py
@@ -0,0 +1,18 @@
+from adventofcode import AoC
+
+
+def part1(inp: list[str]):
+    return None
+
+
+def part2(inp: list[str]):
+    return None
+
+
+aoc = AoC(part_1=part1, part_2=part2)
+inp = """"""
+aoc.assert_p1(inp, None)
+aoc.submit_p1()
+
+aoc.assert_p2(inp, None)
+aoc.submit_p2()
