a5dca5af0def1c2ec5c8fdb4e0c343d6ee7b9fe9
Anže Pečar <anze@pecar.me>
2023-12-25 10:35:11 +0000

    Solve day 25

--- 25.py
+++ 25.py
@@ -1,18 +1,32 @@
+import networkx as nx
 from adventofcode import AoC
 
 
 def part1(inp: list[str]):
-    return None
+    G = nx.Graph()
+    for line in inp:
+        a, b = line.split(": ")
+        for link in b.split(" "):
+            G.add_edge(a, link)
+    comp = nx.community.girvan_newman(G)
+    frst, scnd = tuple(sorted(c) for c in next(comp))
+    print(frst, scnd)
+    return len(frst) * len(scnd)
 
 
-def part2(inp: list[str]):
-    return None
-
-
-aoc = AoC(part_1=part1, part_2=part2)
-inp = """"""
-aoc.assert_p1(inp, None)
+aoc = AoC(part_1=part1)
+inp = """jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr"""
+aoc.assert_p1(inp, 54)
 aoc.submit_p1()
-
-aoc.assert_p2(inp, None)
-aoc.submit_p2()
